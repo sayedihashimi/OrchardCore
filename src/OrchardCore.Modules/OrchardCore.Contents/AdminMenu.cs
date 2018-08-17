5a29ce51a8e83c7f16ec1e0c57ae79fd107c01be
Matías Molleja <matiasmolleja@outlook.com>
2018-08-17 13:39:18 +0200

    Show submenu for each content type

--- src/OrchardCore.Modules/OrchardCore.Contents/AdminMenu.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/AdminMenu.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Localization;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Environment.Navigation;
 using System;
@@ -35,21 +37,37 @@ namespace OrchardCore.Contents
 
             var contentTypeDefinitions = _contentDefinitionManager.ListTypeDefinitions().OrderBy(d => d.Name);
 
-            builder.Add(T["Content"], "1.4", content => content
-                .AddClass("content").Id("content")
-                .Add(T["Content Items"], "1", contentItems => contentItems
-                    .Permission(Permissions.EditOwnContent)
-                    .Action("List", "Admin", new { area = "OrchardCore.Contents" })
-                    .LocalNav())
-                );
+            var creatable = contentTypeDefinitions.Where(ctd => ctd.Settings.ToObject<ContentTypeSettings>().Creatable).OrderBy(ctd => ctd.DisplayName);
+            var listable = contentTypeDefinitions.Where(ctd => ctd.Settings.ToObject<ContentTypeSettings>().Listable).OrderBy(ctd => ctd.DisplayName);
 
-            var contentTypes = contentTypeDefinitions.Where(ctd => ctd.Settings.ToObject<ContentTypeSettings>().Creatable).OrderBy(ctd => ctd.DisplayName);
-            if (contentTypes.Any())
+
+            builder.Add(T["Content"], "1.4", content =>
+            {
+                content.AddClass("content").Id("content")
+               .Add(T["Content Items"], "1", contentItems =>
+               {
+                   contentItems
+                   .LinkToFirstChild(false)
+                   .Permission(Permissions.EditOwnContent)
+                   .Action("List", "Admin", new { area = "OrchardCore.Contents" });
+
+                   foreach (var ctd in listable)
+                   {
+                       var rv = new RouteValueDictionary();
+                       // todo: merge filterbox branch or this won't work yet because the content item list is not ready to read the querystring.
+                       rv.Add("contentType", ctd.Name);
+                       contentItems.Add(new LocalizedString(ctd.DisplayName, ctd.DisplayName), t => t.Action("List", "Admin", "OrchardCore.Contents", rv));
+                   }
+               });
+            });
+
+
+            if (creatable.Any())
             {
                 builder.Add(T["New"], "-1", async newMenu =>
                 {
                     newMenu.LinkToFirstChild(false).AddClass("new").Id("new");
-                    foreach (var contentTypeDefinition in contentTypes)
+                    foreach (var contentTypeDefinition in creatable)
                     {
                         var ci = await _contentManager.NewAsync(contentTypeDefinition.Name);
                         var cim = await _contentManager.PopulateAspectAsync<ContentItemMetadata>(ci);
