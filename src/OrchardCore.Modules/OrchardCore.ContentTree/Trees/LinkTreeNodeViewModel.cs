94868791e612769fda8aa826594ba23b70514e3e
Matías Molleja <matiasmolleja@outlook.com>
2018-09-16 18:43:52 +0200

    wip

--- src/OrchardCore.Modules/OrchardCore.ContentTree/Trees/LinkTreeNodeViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentTree/Trees/LinkTreeNodeViewModel.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace OrchardCore.ContentTree.Trees
 {
     public class LinkTreeNodeViewModel
     {
         public string LinkText { get; set; }
         public string LinkUrl { get; set; }
+        public string Classes { get; set; }
     }
 }
